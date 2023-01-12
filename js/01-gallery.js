7b53bee21e9a50ea29e47cd7ef000a5eaba8a5c2
SoloviovO <soloviov.o.m@gmail.com>
2023-01-12 23:59:57 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const boxGalleryEl = document.querySelector('.gallery');
+const itemGalleryMarkup = createGalleryItemMarkup(galleryItems);
+
+boxGalleryEl.insertAdjacentHTML('beforeend', itemGalleryMarkup);
+
+boxGalleryEl.addEventListener('click', onBoxGalleryClick);
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+function onBoxGalleryClick(event) {
+  event.preventDefault();
+}
+
 console.log(galleryItems);
